e62de17c257e2f069f8ba72c74bf2e9413b05151
khangrey <khan.grey.dev@gmail.com>
2022-11-08 11:46:51 +0600

    checked argument added to the checkbox.

--- resources/views/components/forms/inputs/checkbox.blade.php
+++ resources/views/components/forms/inputs/checkbox.blade.php
@@ -1,16 +1,16 @@
 <div>
-    @if($title)
+    @if ($title)
         <p class="text-base text-gray-800 dark:text-white mb-2.5">{{ $title }}</p>
     @endif
     <div
         class="relative flex items-center py-2.5 px-5 bg-white border border-gray-300 rounded-md dark:bg-slate-900 dark:border-gray-700">
         <label for="{{ $id }}"
-               class="flex-1 min-w-0 text-sm font-medium text-gray-400 w-full hover:cursor-pointer">
+            class="flex-1 min-w-0 text-sm font-medium text-gray-400 w-full hover:cursor-pointer">
             {{ $label }}
         </label>
         <div class="ml-3 flex items-center h-5">
-            <input id="{{ $id }}" name="{{ $name }}" type="checkbox"
-                   class="focus:ring-emerald-400 h-4 w-4 text-emerald-400 border-gray-300 rounded" />
+            <input id="{{ $id }}" name="{{ $name }}" type="checkbox" @checked($checked)
+                class="focus:ring-emerald-400 h-4 w-4 text-emerald-400 border-gray-300 rounded" />
         </div>
     </div>
 </div>
--- src/Components/Forms/Inputs/Checkbox.php
+++ src/Components/Forms/Inputs/Checkbox.php
@@ -13,15 +13,21 @@ class Checkbox extends QazhboardComponent
     public string $id;
     public string $label;
     public ?string $title;
+    public bool $checked;
 
     public function __construct(
-        string $name, string $label, string $id = null, string $title = null
+        string $name,
+        string $label,
+        string $id = null,
+        string $title = null,
+        bool $checked = false
     )
     {
         $this->name = $name;
         $this->id = $id ?: $name;
         $this->label = $label;
         $this->title = $title;
+        $this->checked = $checked;
     }
 
     public function render(): View
